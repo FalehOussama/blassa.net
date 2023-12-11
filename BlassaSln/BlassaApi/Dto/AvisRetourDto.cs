2e55845358166d783fd0646a9041f3baf0567eb2
FalehOussama <FalehOussama.ch@gmail.com>
2023-12-11 17:00:17 +0100

    mes-notes-2

--- BlassaSln/BlassaApi/Dto/AvisRetourDto.cs
+++ BlassaSln/BlassaApi/Dto/AvisRetourDto.cs
@@ -2,7 +2,7 @@
 
 namespace BlassaApi.Dto
 {
-    public class AvisRetourDtocs
+    public class AvisRetourDto
     {
         public int NbreTotal { get; set; }
         public List<Avi>? Avis { get; set; }
