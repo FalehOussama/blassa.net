2c34289151cc776c968f730c47cbf06fb6278cb3
FalehOussama <FalehOussama.ch@gmail.com>
2024-01-29 22:54:06 +0100

    Update packages

--- BlassaSln/BlassaApi/obj/Debug/net6.0/BlassaApi.AssemblyInfo.cs
+++ BlassaSln/BlassaApi/obj/Debug/net6.0/BlassaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlassaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dd3049d1621e8277aeb308089663498b22a55d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlassaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlassaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
