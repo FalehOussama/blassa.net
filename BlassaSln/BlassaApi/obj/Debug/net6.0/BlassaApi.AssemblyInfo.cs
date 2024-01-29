b51e70d6e95df3e4b42d8077bdc21f4f50ab7b68
FalehOussama <FalehOussama.ch@gmail.com>
2024-01-29 21:49:22 +0100

    Deploy Init 3

--- BlassaSln/BlassaApi/obj/Debug/net6.0/BlassaApi.AssemblyInfo.cs
+++ BlassaSln/BlassaApi/obj/Debug/net6.0/BlassaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlassaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d84b71d8e9500e8f5043ff9ef405c21bc27702b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlassaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlassaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
