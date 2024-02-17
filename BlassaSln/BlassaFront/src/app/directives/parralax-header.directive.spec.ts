e51a528d859b88874a85a619e4deca68aed3267e
FalehOussama <FalehOussama.ch@gmail.com>
2024-02-17 09:33:07 +0100

    test ParralaxHeaderDirective

--- BlassaSln/BlassaFront/src/app/directives/parralax-header.directive.spec.ts
+++ BlassaSln/BlassaFront/src/app/directives/parralax-header.directive.spec.ts
@@ -1,8 +1,20 @@
+import { Renderer2 } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { DomController } from '@ionic/angular';
 import { ParralaxHeaderDirective } from './parralax-header.directive';
 
 describe('ParralaxHeaderDirective', () => {
   it('should create an instance', () => {
-    const directive = new ParralaxHeaderDirective();
+    let elRefMock = {
+      nativeElement: document.createElement('div')
+    };
+
+    let fixture = TestBed.createComponent(ParralaxHeaderDirective);
+    let renderer2 = fixture.componentRef.injector.get<Renderer2>(Renderer2);
+
+    let domCtrl = new DomController();
+
+    const directive = new ParralaxHeaderDirective(elRefMock, renderer2, domCtrl);
     expect(directive).toBeTruthy();
   });
 });
